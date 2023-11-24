235bd31faec6ea7efd8901bbc7fe5afc3c31eea6
Cindy-Coulibaly <98350788+Cindy-Coulibaly@users.noreply.github.com>
2023-11-24 15:15:24 -0500

    created the interface needed for every class

---
+++ Cryptography/Cryptography/Interfaces/ICryptography.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography.Interfaces
+{
+    internal interface ICryptography
+    {
+        public string Encrypt(string plaintext);
+        public string Decrypt(string encryptedText);
+        public void GenerateKey();
+    }
+}
