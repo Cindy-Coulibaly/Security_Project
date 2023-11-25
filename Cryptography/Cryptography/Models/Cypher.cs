64bc9934545fc32bc596a8f1276eb2b86b2978dc
Cindy-Coulibaly <98350788+Cindy-Coulibaly@users.noreply.github.com>
2023-11-25 17:16:20 -0500

    started the cypher class by making a backing field, name key and created 2 contructor, main delete one, but one takes in a key and a another doesn't.

---
+++ Cryptography/Cryptography/Models/Cypher.cs
@@ -0,0 +1,55 @@
+﻿using Cryptography.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography.Models
+{
+    internal class Cypher : ICryptography
+    {
+        private int _key;
+        private const int MINKEY = -26;
+        private const int MAXKEY = -26;
+
+        public Cypher()
+        {
+            
+        }
+        public Cypher(int key)
+        {
+            Key = key;
+        }
+        public int Key 
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                if(value>MAXKEY && value < MINKEY)
+                {
+                    throw new ArgumentException("you can't cypher more then 26 and less then -26 ");
+                }
+                _key = value;
+            }
+        }
+        public string Decrypt(string encryptedText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encrypt(string plaintext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateKey()
+        {
+            // let the user input a key, it's definitly not random
+            throw new NotImplementedException();
+        }
+    }
+}
